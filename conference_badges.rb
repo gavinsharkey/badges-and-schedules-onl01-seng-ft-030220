03f2aa131733b759ee50cf091f7156847be431d9
Gavin Sharkey <gavinsharkey2016@gmail.com>
2020-03-09 17:01:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,7 +13,13 @@ def assign_rooms(attendees)
   end
 end
 
+<<<<<<< HEAD
 def printer(attendees)
   batch_badge_creator(attendees).each { |val| puts val }
   assign_rooms(attendees).each { |val| puts val }
+=======
+def conference_badges(attendees)
+  batch_badge_creator(attendees).each { |val| puts val}
+  assign_rooms(attendees).each { |val| puts val}
+>>>>>>> 7e51e8cf0f4b0a95e4fd665dfec5c5b881a99ed4
 end
